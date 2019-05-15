d319d6cc6064d849671d6922ae9385cba39754b2
davidjoliver86 <davidjoliver86@gmail.com>
2019-05-14 23:26:24 -0700

    added lines of code; deleted other lines of code

--- pytodotxt/cli.py
+++ pytodotxt/cli.py
@@ -1,16 +1,54 @@
+import sys
 import click
 
-from . import config
 from . import todotxt
+from . import config
+
+
+def error(msg):
+    click.secho(msg, fg='red')
+    sys.exit(1)
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--config-file')
+@click.pass_context
+def cli(ctx, config_file):
+    config.load_from_file(config_file)
 
 
-@cli.command()
-def list():
-    todos = todotxt.read_file(config.TODOTXT_FILE)
-    for todo in todos:
-        print(f'{todo.id:>5d} {todo.text}')
+@cli.command(name='list')
+@click.option('--sort-order', default='priority')
+@click.option('--show-done', is_flag=True, default=False)
+@click.pass_context
+def _list(ctx, show_done, sort_order):
+    todos = todotxt.read_file(config.TODO_FILE)
+    sorted_todos = _sort(todos, sort_order)
+    for todo in sorted_todos:
+        if not show_done and todo.completed:
+            continue
+        print(f'{todo.text}')
+
+def _sort(todos, sort_order):
+    """
+    Builds a list alongside each Todo that acts as the sort key.
+
+    Options for sort_order:
+    - priority: Sort by priority only.
+    - context: Sort by context(s) only.
+    - priority,context: Sort by priority, then subsort by context(s).
+    - context,priority: Sort by context(s), then subsort by priority.
+    """
+    sorting = sort_order.split(',')
+    todos_with_key = list(zip(todos, [list() for i in range(len(todos))]))
+    for sort_type in sorting:
+        if sort_type == 'priority':
+            for todo, key in todos_with_key:
+                key.append(todo.priority or '')
+        elif sort_type == 'context':
+            for todo, key in todos_with_key:
+                key.append(todo.contexts)
+        else:
+            error(f'Unsupported sort type: {sort_type}')
+    return [x[0] for x in sorted(todos_with_key, key=lambda todo: todo[1])]
+
--- pytodotxt/config.py
+++ pytodotxt/config.py
@@ -1,1 +1,28 @@
-TODOTXT_FILE = '/home/davido/Sync/todo.txt'
+import click
+import sys
+import configparser
+from pathlib import Path
+
+DEFAULT_CONFIG_LOCATION = '~/.config/pytodotxt.ini'
+DEFAULT_CONFIG = """[pytodotxt]
+todo_file = ~/todo.txt
+"""
+
+def load_from_file(config_path: str) -> None:
+    if config_path:
+        config_path = Path(config_path).expanduser()
+    else:
+        config_path = Path(DEFAULT_CONFIG_LOCATION).expanduser()
+    if not config_path.exists():
+        config_path.write_text(DEFAULT_CONFIG)
+        click.secho(f'Created a config file: {config_path}; edit the todo_file and rerun PyTodoTxt.', fg='yellow')
+        sys.exit(0)
+    parser = configparser.ConfigParser()
+    parser.read([config_path])
+    for section in parser.sections():
+        for key in parser[section]:
+            globals()[key.upper()] = parser[section][key]
+
+# Configs
+
+TODO_FILE = ''
